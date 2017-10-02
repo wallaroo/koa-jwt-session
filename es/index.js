259018e7d6686995ead8b046193f8e811fb802ef
Michele <michele.giacomelli@gmail.com>
2017-10-02 19:53:43 +0200

    first commit

---
+++ es/index.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import omit from "lodash.omit";
+const defOpts = { expiresIn: "1h", cookie: "jwt-session" };
+export default function createMiddleware(secret, opts = { expiresIn: "1h", cookie: "jwt-session" }) {
+    opts = Object.assign({}, defOpts, opts);
+    return async (ctx, next) => {
+        const token = ctx.cookies.get(opts.cookie);
+        if (token) {
+            try {
+                ctx.session = omit(jwt.verify(token, secret), "exp", "iat");
+            } catch (e) {
+                //if the token is expired no session will be load
+                if (e.name !== "TokenExpiredError") {
+                    ctx.status = 403;
+                    return;
+                }
+            }
+        }
+        ctx.session = ctx.session || {};
+        await next();
+        if (ctx.session) {
+            console.log("SESSION", ctx.session);
+            const newToken = jwt.sign(ctx.session, secret, omit(opts, "cookie"));
+            ctx.cookies.set(opts.cookie, newToken);
+        }
+    };
+}
